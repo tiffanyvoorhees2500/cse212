8d6ce9f3174dc7e4547f7f18741b8c67a8a4e11c
tiffanyvoorhees2500 <126093924+tiffanyvoorhees2500@users.noreply.github.com>
2025-03-18 09:51:16 -0600

    Learning Activities Completed

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -24,7 +24,7 @@ public class Translator
     /// <returns>fixed array of divisors</returns>
     public void AddWord(string fromWord, string toWord)
     {
-        // ADD YOUR CODE HERE
+        _words.TryAdd(fromWord, toWord);
     }
 
     /// <summary>
@@ -35,6 +35,6 @@ public class Translator
     public string Translate(string fromWord)
     {
         // ADD YOUR CODE HERE
-        return "";
+        return _words.TryGetValue(fromWord, out var toWord) ? toWord : "???";
     }
 }
