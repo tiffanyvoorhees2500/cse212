8b5fbb08ae733cb33ad6e3074bdbaa964803bcf2
tiffanyvoorhees2500 <126093924+tiffanyvoorhees2500@users.noreply.github.com>
2025-03-20 14:39:22 -0600

    Week 3: Problem 2 Completed: Degree Summary

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -64,7 +64,12 @@ public static class SetsAndMaps
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            var degree = fields[3];
+            if (degrees.ContainsKey(degree)){
+                degrees[degree]++;
+            } else {
+                degrees[degree] = 1;
+            }
         }
 
         return degrees;
